0af4e6eef374c8c4a1bf0027bad70cb484209a57
José Nery <josenerydev@gmail.com>
2020-08-19 00:49:43 -0300

    add User and Places controllers

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const usersRoutes = require('./routes/users-routes');
+const placesRoutes = require('./routes/places-routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/users', usersRoutes);
+app.use('/api/places', placesRoutes);
+
+app.use((error, req, res, next) => {
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((conn) => app.listen(process.env.PORT))
+  .catch((err) => console.error(err));
